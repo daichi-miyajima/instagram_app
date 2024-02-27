43d5a7eb60aca80e0ac791fe8547ab02e9edbd10
daichi-miyajima <dm.456ww@gmail.com>
2024-02-27 23:26:05 +0900

    my_pageのユーザ画像をCircleAvatarに修正

--- lib/mypage/my_page.dart
+++ lib/mypage/my_page.dart
@@ -29,9 +29,9 @@ class MyPage extends StatelessWidget {
                     padding: const EdgeInsets.all(16),
                     child: Row(
                       children: [
-                        Image.network(
-                          mymodel.imageURL ?? 'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=',
-                          height: 60,
+                        CircleAvatar(
+                          radius: 50, // サイズを調整する値を設定します
+                          backgroundImage: NetworkImage(mymodel.imageURL ?? 'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI='), // ユーザー画像
                         ),
                         Padding(
                           padding: const EdgeInsets.all(16),
