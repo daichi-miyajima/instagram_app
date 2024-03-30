793924e6547043ba354a94ba89541e3e0595a184
daichi-miyajima <dm.456ww@gmail.com>
2024-03-30 20:14:15 +0900

    マイページでTabBarで各genreの投稿を表示

--- lib/mypage/my_page.dart
+++ lib/mypage/my_page.dart
@@ -8,13 +8,6 @@ import 'package:provider/provider.dart';
 
 class MyPage extends StatelessWidget {
   MyPage({Key? key}) : super(key: key);
-  // image一覧
-  final images = [
-    'https://english-club.jp/wp-content/uploads/2019/07/english-brain-1024x717.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvoSuexQoraIJ3Xc8Qde7Y_A-7v7vfwWxveA&usqp=CAU',
-    'https://www.pakutaso.com/shared/img/thumb/kaigoIMGL8113.jpg',
-    'https://news.value-press.com/wp-content/uploads/interview_top_image_pakutaso.jpg',
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,127 +15,203 @@ class MyPage extends StatelessWidget {
       create: (_) => MyModel()..fetchUser(),
       child: Consumer<MyModel>(
         builder: (context, mymodel, child) {
-          return Scaffold(
-            appBar: Header(),
-            body: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // 左寄せ
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 50, // サイズを調整する値を設定します
-                          backgroundImage: NetworkImage(mymodel.imageURL ?? 'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI='), // ユーザー画像
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start, // 左寄せ
-                            children: [
-                              Text(
-                                mymodel.name ?? '名前なし',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
+          return DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: Header(),
+              body: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // 左寄せ
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 50, // サイズを調整する値を設定します
+                            backgroundImage: NetworkImage(mymodel.imageURL ?? 'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI='), // ユーザー画像
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start, // 左寄せ
+                              children: [
+                                Text(
+                                  mymodel.name ?? '名前なし',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                '@dacchiman',
-                              ),
-                              Text(
-                                '#映画#ドラマ#旅行',
-                                style: TextStyle(
-                                  color: Colors.blue,
+                                Text(
+                                  '@dacchiman',
                                 ),
-                              ),
-                            ],
+                                Text(
+                                  '#映画#ドラマ#旅行',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 16),
+                          const SizedBox(width: 16),
+                          Spacer(),
+                          IconButton(
+                            onPressed: () async {
+                              // 画面遷移
+                              await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => EditProfilePage(mymodel.name!),
+                                ),
+                              );
+                              mymodel.fetchUser();
+                            },
+                            icon: Icon(Icons.edit),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextButton(
+                            onPressed: () async {
+                              // ログアウト
+                              await mymodel.logout();
+                              // Navigator.of(context).pop();
+                              await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginPage(),
+                                  fullscreenDialog: true,
+                                ),
+                              );
+                            },
+                            child: Text('ログアウト'),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      color: Colors.grey,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text(
+                              'My Talk Best5'
                           ),
                         ),
-                        const SizedBox(width: 16),
-                        const SizedBox(width: 16),
-                        Spacer(),
-                        IconButton(
-                          onPressed: () async {
-                            // 画面遷移
-                            await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => EditProfilePage(mymodel.name!),
-                              ),
-                            );
-                            mymodel.fetchUser();
-                          },
-                          icon: Icon(Icons.edit),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextButton(
-                          onPressed: () async {
-                            // ログアウト
-                            await mymodel.logout();
-                            // Navigator.of(context).pop();
-                            await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => LoginPage(),
-                                fullscreenDialog: true,
-                              ),
-                            );
-                          },
-                          child: Text('ログアウト'),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    color: Colors.grey,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
-                        child: Text(
-                          'My Talk Best5'
-                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.height,
-                    child: ListView(
-                      children: (mymodel.myFeeds!.toList()
-                        ..sort((a, b) => a.rank.compareTo(b.rank)))
-                          .map<Widget>((feed) => ListTile(
-                        title: Row(
-                          children: [
-                            Text(
-                              feed.rank.toString(),
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(width: 10),
-                            Text(
-                              feed.title,
-                              textAlign: TextAlign.left,
-                            ),
-                          ],
+                    // タブでカテゴリ毎に切り替え
+                    TabBar(
+                      tabs: [
+                        Tab(
+                          icon: Icon(Icons.movie), // 映画のアイコン
+                          text: '映画',
                         ),
-                        subtitle: null,
-                      ),
-                      ).toList(),
+                        Tab(
+                          icon: Icon(Icons.travel_explore), // 旅行のアイコン
+                          text: '旅行',
+                        ),
+                        Tab(
+                          icon: Icon(Icons.spa), // 旅行のアイコン
+                          text: 'サウナ',
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                    // タブ1のコンテンツ
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.5, // タブの高さに合わせて調整
+                      child: TabBarView(
+                        children: [
+                          // タブ1のコンテンツ
+                          ListView(
+                            children: (mymodel.myFeeds!.where((feed) => feed.genre == 'movie').toList()
+                              ..sort((a, b) => a.rank.compareTo(b.rank)))
+                                .map<Widget>((feed) => ListTile(
+                              title: Row(
+                                children: [
+                                  Text(
+                                    feed.rank.toString(),
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    feed.title,
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ],
+                              ),
+                              subtitle: null,
+                            ),
+                            ).toList(),
+                          ),
+                          // タブ2のコンテンツ
+                          ListView(
+                            children: (mymodel.myFeeds!.where((feed) => feed.genre == 'travel').toList()
+                              ..sort((a, b) => a.rank.compareTo(b.rank)))
+                                .map<Widget>((feed) => ListTile(
+                              title: Row(
+                                children: [
+                                  Text(
+                                    feed.rank.toString(),
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    feed.title,
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ],
+                              ),
+                              subtitle: null,
+                            ),
+                            ).toList(),
+                          ),
+                          // タブ3のコンテンツ
+                          ListView(
+                            children: (mymodel.myFeeds!.where((feed) => feed.genre == 'sauna').toList()
+                              ..sort((a, b) => a.rank.compareTo(b.rank)))
+                                .map<Widget>((feed) => ListTile(
+                              title: Row(
+                                children: [
+                                  Text(
+                                    feed.rank.toString(),
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    feed.title,
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ],
+                              ),
+                              subtitle: null,
+                            ),
+                            ).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            )
           );
         },
       ),
